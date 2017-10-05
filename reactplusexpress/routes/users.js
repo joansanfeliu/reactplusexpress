d5a8a44f8489e511ec5a97ccb0b56319677194d3
Joan <sanfeliu.joan@gmail.com>
2017-10-05 10:14:26 +0200

    Initial commit

--- reactplusexpress/routes/users.js
+++ reactplusexpress/routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+
+  res.json([{
+    id: 1,
+    username: "John"
+  }, {
+    id: 2,
+    username: "Peter"
+  }]);
 });
 
 module.exports = router;
